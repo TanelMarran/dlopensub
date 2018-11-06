07e3c190ed44a3b08065f094b40652fbea14dd0e
Tanel Lenovo Laptop <dashpenguin5@gmail.com>
2018-11-06 15:32:03 +0200

    Tegin töötavaks

--- projekt.py
+++ projekt.py
@@ -1,13 +1,25 @@
-from xmlrpc.client import *
+import xmlrpc.client
 import base64
 import gzip
 
-server = ServerProxy("http://api.opensubtitles.org/xml-rpc")
-##for method in server.system.listMethods():
-##    print(method)
-##    print(server.system.methodHelp(method))
-    
-useragent = "utopensub"
-log = server.LogIn("", "", "en", useragent))
+server = xmlrpc.client.ServerProxy("http://api.opensubtitles.org/xml-rpc")
 
-DownloadSubtitles("utopensub"())
+useragent = "utopensub"
+log = server.LogIn("","","en",useragent)
+token = log["token"]
+
+nimi = input("Sisestage filmi nimi: ")
+info = server.SearchSubtitles(token, [{"sublanguageid":"eng", "query":nimi}])
+
+if info["data"] != []:
+    #for i,j in info["data"][0].items():
+    #    print(str(i)+": "+str(j))
+    fail = server.DownloadSubtitles(token,[info["data"][0]["IDSubtitleFile"]])
+    fail = fail["data"][0]["data"]
+    fail = base64.b64decode(fail)
+    #print(fail)
+    fail = gzip.decompress(fail)
+    print(type(fail))
+    print(fail)
+    with open("sub.srt", 'wb') as f:
+        f.write(fail)
